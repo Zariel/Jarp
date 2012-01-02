8d8a356a65d89c0eae33d3772acf3512990246de
Chris Bannister <c.bannister@gmail.com>
2012-01-02 17:37:25 +0000

    Test the new verifyier

--- src/com/fein/jarp/Jarp.java
+++ src/com/fein/jarp/Jarp.java
@@ -1,24 +1,45 @@
 package com.fein.jarp;
 
 import com.fein.jarp.par2.ParFile;
-import com.fein.jarp.par2.packets.Packet;
+import com.fein.jarp.par2.packets.AbstractPacket;
+import com.fein.jarp.par2.verifier.Par2Verifier;
 
 public class Jarp {
+
+	public static final int NUM_CPUS = Runtime.getRuntime()
+			.availableProcessors();
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		ParFile par = new ParFile(
-				"C:\\Users\\Chris\\Downloads\\fight.club.1999.remastered.repack.720p.bluray.vol000+01.PAR2");
+				"D:\\dev\\jarp\\test\\Bored to Death - 1x01 - Stockholm Syndrome.avi.par2");
 
-		Packet p = null;
-		int packets = par.getNumPackets();
+		AbstractPacket p = null;
+		// int packets = par.getNumPackets();
+		//
+		// for(int i = 0; i < packets; i++) {
+		// p = par.getPacket(i);
+		//
+		// if(p.getPacketType() == PacketType.FILE_DESC) {
+		// FileDesc body = (FileDesc) p.getBody();
+		// System.out.println(body.getFileName());
+		// } else if(p.getPacketType() == PacketType.MAIN_PACKET) {
+		// MainPacket m = (MainPacket) p.getBody();
+		// }
+		// }
 
-		for(int i = 0; i < packets; i++) {
-			p = par.getPacket(i);
+		Par2Verifier v = new Par2Verifier(par);
 
-			System.out.println(p.getHeader().getPacketType());
+		try {
+			v.verifyFiles();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+
+		System.out.println(par.isPacketsValid());
+		System.out.println(par.getNumFiles());
+		System.out.println(par.getCreator());
 	}
 }
