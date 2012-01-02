90788500ab78d8c517391caff41ed1b1af04a27f
Chris Bannister <c.bannister@gmail.com>
2012-01-02 17:33:43 +0000

    Move those static byte methods into its own package, they dont belong in AbstractPacket.

---
+++ src/com/fein/jarp/util/ByteUtil.java
@@ -0,0 +1,44 @@
+package com.fein.jarp.util;
+
+import java.math.BigInteger;
+
+
+
+public class ByteUtil {
+
+	public static BigInteger byteToUint8(byte[] input, int offset) {
+		return ByteUtil.byteToUint8(input, offset, 8);
+	}
+
+	public static BigInteger byteToUint8(byte[] input, int offset, int len) {
+		BigInteger num = BigInteger.ZERO;
+	
+		for(int i = offset; i < (offset + len); i++) {
+			BigInteger n = new BigInteger(
+					Integer.toString((input[i] & 0x000000ff) << (i * 8)));
+			num = num.or(n);
+		}
+	
+		return num;
+	}
+
+	public static long byteToUint4(byte[] input, int offset) {
+		return ByteUtil.byteToUint4(input, offset, 4);
+	}
+
+	public static long byteToUint4(byte[] input, int offset, int len) {
+		long num = 0;
+	
+		for(int i = offset; i < (offset + len); i++) {
+			num |= ((input[i] & 0x000000ff) << (i * 8));
+		}
+	
+		return num;
+	
+	}
+
+	public static BigInteger getPacketSize(byte[] data) {
+		return byteToUint8(data, 8, 8);
+	}
+
+}
